da58c69c02d5b5a70814b5845c4b404c19ad2e4e
yashwanth2706 <yash2706.git@gmail.com>
2026-03-11 11:27:40 +0530

    Create state first app and add appState

---
+++ list-app-state-first/js/state.js
@@ -0,0 +1,9 @@
+const appState = {
+    editingItem : null,
+    originalText : null,
+    isInEditMode : false,
+    isInDeleteMode : false,
+    confirmClear : false,
+    confirmDelete : false,
+    items : []
+}
